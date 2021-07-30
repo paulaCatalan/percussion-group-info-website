c743604061b530736b4bd5d5b3cca2a5f767dc49
Francisco Goncalves <franciscogoncalves@Franciscos-MacBook-Pro.local>
2021-07-30 10:39:55 +0200

    add calendar events from json with javascript, add basic styling to events

--- client/calendar/calendar.js
+++ client/calendar/calendar.js
@@ -1,3 +1,5 @@
+const eventList = document.getElementById('eventList')
+console.log(eventList)
 
 async function fetchEventsJSON() {
     const response = await fetch('../db/events.json');
@@ -6,6 +8,10 @@ async function fetchEventsJSON() {
 }
 
 fetchEventsJSON().then(events => {
-    console.log(events)
+    events.forEach((event) => {
+        eventList.innerHTML += '<div class="event">' + '<h3>' + event.title + '</h3>' +
+        '<p>' + event.description + '</p>' +
+        '<ul>' + '<li>' + event.members + ' membres' + '</li>' + '<li>' + event.date + '</li>' + '<li>' + event.time + '</li>' + '<li>' + event.location + '</li>' + '</ul>' + '</div>'
+    })
 });
 
